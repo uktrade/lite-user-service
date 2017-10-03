92cfe7d70cd44e8651a6e20ecc92c1c559290ccd
Laurence Cartwright <laurence.cartwright@fivium.co.uk>
2017-10-03 15:54:32 +0100

    Reuse instance of XPath where thread-safe

--- src/main/java/uk/gov/bis/lite/user/spire/SpireUserRolesParser.java
+++ src/main/java/uk/gov/bis/lite/user/spire/SpireUserRolesParser.java
@@ -6,13 +6,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.w3c.dom.Node;
 import uk.gov.bis.lite.common.spire.client.SpireResponse;
+import uk.gov.bis.lite.common.spire.client.exception.SpireClientException;
 import uk.gov.bis.lite.common.spire.client.parser.SpireParser;
 import uk.gov.bis.lite.user.spire.SpireUserRole.SpireUserRoleBuilder;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 
 public class SpireUserRolesParser implements SpireParser<List<SpireUserRole>> {
   private static final Logger LOGGER = LoggerFactory.getLogger(SpireUserRolesParser.class);
@@ -33,14 +40,15 @@ public class SpireUserRolesParser implements SpireParser<List<SpireUserRole>> {
         .map(node -> {
           Node clonedNode = node.cloneNode(true); // Better XPath performance with large DOMs
           if (StringUtils.equals(clonedNode.getNodeName(), "ROLE")) {
+            XPath xpath = XPathFactory.newInstance().newXPath();
             SpireUserRoleBuilder userRoleBuilder = SpireUserRole.builder();
-            SpireResponse.getNodeValue(clonedNode,"RES_TYPE").ifPresent(userRoleBuilder::setResType);
-            SpireResponse.getNodeValue(clonedNode,"ROLE_NAME").ifPresent(userRoleBuilder::setRoleName);
-            SpireResponse.getNodeValue(clonedNode,"FULL_NAME").ifPresent(userRoleBuilder::setFullName);
-            SpireResponse.getNodeValue(clonedNode,"SAR_REF").ifPresent(userRoleBuilder::setSarRef);
-            SpireResponse.getNodeValue(clonedNode,"SITE_REF").ifPresent(userRoleBuilder::setSiteRef);
-            SpireResponse.getNodeValue(clonedNode,"IS_ADMIN").ifPresent(userRoleBuilder::setIsAdmin);
-            SpireResponse.getNodeValue(clonedNode,"IS_APPLICANT").ifPresent(userRoleBuilder::setIsApplicant);
+            getNodeValue(xpath, clonedNode,"RES_TYPE").ifPresent(userRoleBuilder::setResType);
+            getNodeValue(xpath, clonedNode,"ROLE_NAME").ifPresent(userRoleBuilder::setRoleName);
+            getNodeValue(xpath, clonedNode,"FULL_NAME").ifPresent(userRoleBuilder::setFullName);
+            getNodeValue(xpath, clonedNode,"SAR_REF").ifPresent(userRoleBuilder::setSarRef);
+            getNodeValue(xpath, clonedNode,"SITE_REF").ifPresent(userRoleBuilder::setSiteRef);
+            getNodeValue(xpath, clonedNode,"IS_ADMIN").ifPresent(userRoleBuilder::setIsAdmin);
+            getNodeValue(xpath, clonedNode,"IS_APPLICANT").ifPresent(userRoleBuilder::setIsApplicant);
             return userRoleBuilder.build();
           } else {
             return null;
@@ -49,4 +57,13 @@ public class SpireUserRolesParser implements SpireParser<List<SpireUserRole>> {
         .filter(Objects::nonNull)
         .collect(Collectors.toList());
   }
+
+  private Optional<String> getNodeValue(XPath xpath, Node node, String name){
+    try {
+      return Optional.ofNullable(((Node) xpath.evaluate(name, node, XPathConstants.NODE))).map(Node::getTextContent);
+    } catch (XPathExpressionException e) {
+      throw new SpireClientException("Error occurred while parsing the SOAP response body", e);
+    }
+  }
+
 }
